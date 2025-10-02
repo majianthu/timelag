5a0546f3775a1aa844db36b3e42020e33a778a31
MA Jian <majian1979@qq.com>
2025-10-02 09:10:12 +0800

    Add files via upload

--- sim2.r
+++ sim2.r
@@ -15,14 +15,14 @@ for(lag in 1:lag1){
   }
 } #lag
 
-x11(width = 12, height = 6)
-plot(x, ylab = "X,Y", col = 2, pch = 20); lines(x,col=2)
-points(y, col = 3, pch = 20); lines(y,col=3)
-legend(0, max(x), legend = c("X","Y"), col = c(2,3), pch = "-")
-
 x11();
-plot(te2[1,],xlab = "lag", ylab = "TE", ylim = c(min(te2),max(te2)),col=1);lines(te2[1,],col=1)
-points(te2[2,],col=2);lines(te2[2,],col=2)
-points(te2[3,],col=3);lines(te2[3,],col=3)
-points(te2[4,],col=4);lines(te2[4,],col=4)
-legend(7,max(te2),legend = c("lag=1","lag=2","lag=3","lag=4"),col = 1:4,pch = "-")
+plot(te2[1,],xlab = "lag", ylab = "TE", ylim = c(min(te2),max(te2)),col=1,pch = 1);lines(te2[1,],col=1)
+points(te2[2,],col=2,pch=2);lines(te2[2,],col=2)
+points(te2[3,],col=3,pch=5);lines(te2[3,],col=3)
+points(te2[4,],col=4,pch=22);lines(te2[4,],col=4)
+legend(7,max(te2),legend = c("l=1","l=2","l=3","l=4"),col = 1:4,pch = c(1,2,5,22))
+
+x11(width = 15, height = 6)
+plot(x, xlab = "i", ylab = "X,Y", ylim = c(min(x,y),max(x,y)), col = 2, pch = 1); lines(x,col=2)
+points(y, col = 3, pch = 20); lines(y,col=3)
+legend(-15, max(x,y), legend = c("X","Y"), col = c(2,3), pch = c(1,20))
